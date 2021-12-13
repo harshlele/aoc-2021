d2d003cf4a2ec3d11c868b368531735b8004de2f
Harshal Lele <harshallele97@gmail.com>
2021-12-13 18:33:59 +0530

    day 12 part 2

--- day12/day12.go
+++ day12/day12.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	content, err := ioutil.ReadFile("day12/test-input.txt")
+	content, err := ioutil.ReadFile("day12/input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -40,29 +40,50 @@ func main() {
 		edges = append(edges, e)
 	}
 
+	//indicates whether ANY lowercase letter has been repeated in the current path(ie stack)
 	twiceVisited := false
-	utils.X(twiceVisited)
-	fmt.Println(calcRes(graph, "start", []string{}))
+	fmt.Println(calcRes(graph, "start", []string{}, &twiceVisited))
 
 }
 
-func calcRes(graph map[string][]string, node string, stack []string) int {
-
+func calcRes(graph map[string][]string, node string, stack []string, p2Case *bool) int {
+	//if node is end, go back
 	if node == "end" {
 		return 1
 	}
 	sum := 0
+
 	stack = append(stack, node)
 
 	for _, n := range graph[node] {
+		//never revisit the start
+		if n == "start" {
+			continue
+		}
+
 		if n != strings.ToLower(n) {
-			sum += calcRes(graph, n, stack)
+			sum += calcRes(graph, n, stack, p2Case)
 		} else {
-			if utils.CountInArr(stack, n) == 0 {
-				sum += calcRes(graph, n, stack)
+			//if its a lower case letter, if it has never been visited, just visit it
+			//if it has been visited once, and there is no letter in the current path that has been visited twice, visit it
+			cnt := utils.CountInArr(stack, n)
+			if cnt == 0 {
+				sum += calcRes(graph, n, stack, p2Case)
+			} else if cnt == 1 && !(*p2Case) {
+				*p2Case = true
+				sum += calcRes(graph, n, stack, p2Case)
 			}
 		}
 	}
+
+	//IMPORTANT
+	//when backtracking, if the current node was the one that got visited twice
+	//and thus set p2Case to true, set it to false again when popping the node off the stack
+	last := stack[len(stack)-1]
+	if last == strings.ToLower(last) && utils.CountInArr(stack, last) == 2 {
+		*p2Case = false
+	}
 	stack = stack[:len(stack)-1]
+
 	return sum
 }
